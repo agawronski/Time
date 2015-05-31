26318be4c1ba91213de754524e39b8588ba22d16
agawronski <aidangawronski@gmail.com>
2015-05-30 23:14:21 -0400

    Test variables for stationarity. Difference them if they are not.

--- quantmodData.R
+++ quantmodData.R
@@ -1,5 +1,5 @@
 setwd("C:/Users/aidan/desktop/Time")
-
+set.seed(1111)
 #This will pull the IBM and Yahoo data using the Quantmod package
 
 library(quantmod)
@@ -33,3 +33,70 @@ IBM <- IBM.uA
 
 AAPL$AAPL.Interest <- sample(50, size=nrow(AAPL), replace=TRUE)
 IBM$IBM.Interest <- sample(50, size=nrow(IBM), replace=TRUE)
+
+reg <- lm(AAPL$AAPL.Close ~ AAPL$AAPL.Interest)
+summary(reg)
+
+# playing around with the lag function:
+# 
+# head(AAPL$AAPL.Interest)
+# head(AAPL$AAPL.Interest <- lag(AAPL$AAPL.Interest, k=-1))
+# tail(AAPL$AAPL.Interest)
+# AAPL <- AAPL[-nrow(AAPL),]
+
+chartSeries(log(AAPL))
+chartSeries(AAPL)
+
+library(vars)
+
+####Augmented Dickey-Fuller (ADF) 
+#testing if the data is stationary ... the null is non-stationary
+#small p-values we reject the null
+#large p-values we fail to reject the null 
+#(and should transform the data if we require it to be stationary)
+adf.test(AAPL$AAPL.Close, alternative="stationary")
+
+
+# https://www.otexts.org/fpp/8/1
+# The following code can be used to find how to make a seasonal series stationary. 
+# The resulting series stored as xstar has been differenced appropriately.
+# 
+# ns <- nsdiffs(x)
+# if(ns > 0) {
+#   xstar <- diff(x,lag=frequency(x),differences=ns)
+# } else {
+#   xstar <- x
+# }
+# nd <- ndiffs(xstar)
+# if(nd > 0) {
+#   xstar <- diff(xstar,differences=nd)
+# }
+#   
+# ***nsdiffs & ndiffs require the forcast package
+library(forecast)
+
+ndiffs(AAPL$AAPL.Close)
+AAPL$AAPL.Close <- diff(AAPL$AAPL.Close, differences=1)
+
+
+adf.test(IBM$IBM.Close, alternative="stationary")
+ndiffs(IBM$IBM.Close)
+IBM$IBM.Close <- diff(IBM$IBM.Close, differences=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
